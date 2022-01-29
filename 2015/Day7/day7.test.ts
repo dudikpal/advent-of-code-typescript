d80b4e84750601de09c67b18bec86bee7fc15b67
Dudik Pál <dudikpal@gmail.com>
2022-01-29 22:41:50 +0100

    #1: 2015 Day7 done (restart from scratch)

--- 2015/Day7/day7.test.ts
+++ 2015/Day7/day7.test.ts
@@ -5,18 +5,24 @@ const file = readFileSync('2015/Day7/input.txt', 'utf-8');
 
 const testCases = [
     /*{input: `123 -> x
+456 -> y
+x AND y -> d
 x OR y -> e
 x LSHIFT 2 -> f
-x AND y -> d
 y RSHIFT 2 -> g
 NOT x -> h
 NOT y -> i
-456 -> y`, expected: 65079, from: 'i'},*/  // value of 'h'
-    {input: file, expected: 956, from: 'a'}
+1 AND i -> j
+j -> k`, expected: 65079, from: 'i'},*/  // value of 'h'
+    {input: file, expectedPart1: 956, expectedPart2: 40149, from: 'a'}
 ];
 
-testCases.forEach(({input, expected, from}) => {
-    it(`expected :${expected}`, () => {
-        expect(day7(input, from)).toEqual(expected)
-    })
+testCases.forEach(({input, expectedPart1, expectedPart2, from}) => {
+    it(`expected part 1 :${expectedPart1}`, () => {
+        expect(day7(input, from).part1).toEqual(expectedPart1);
+    });
+
+    it(`expected part 2 :${expectedPart2}`, () => {
+        expect(day7(input, from).part2).toEqual(expectedPart2);
+    });
 });
--- 2015/Day7/day7.ts
+++ 2015/Day7/day7.ts
@@ -1,170 +1,78 @@
-type Command = {
-    param1: string;
-    param2?: string;
-    command: string;
+type Operation = {
     target: string;
-}
-
-export function day7(input: string, from: string) {
-
-    let lines = input.split(/\n|\r\n/);
-    const starters = lines.filter(line => line.match(/^\d+\s->/));
-    const values = new Map<string, number>();
-
-    let command;
-    for (const starter of starters) {
-        command = parseCommand(starter, values);
-        commandProcessor(command, values);
-        lines.splice(lines.indexOf(starter), 1);
-    }
-
-    while (lines.length > 0) {
-
-        for (const line of lines) {
-            command = parseCommand(line, values);
-            if (command) {
-                commandProcessor(command, values);
-                lines.splice(lines.indexOf(line), 1);
-            }
-        }
-    }
-
-    return values.get(from);
+    args: any[];
+    command: string;
 }
 
 
-function parseCommand(commandLine: string, values: Map<string, number>): Command {
-    let command;
+const METHODS = {
+    AND: (a, b) => (a & b),
+    OR: (a, b) => (a | b),
+    NOT: a => new Uint16Array([~a])[0],
+    LSHIFT: (a, b) => (a << b),
+    RSHIFT: (a, b) => (a >> b)
+};
 
-    if (commandLine.match(/^\d+\s[A-Z]/)) {
-        command = 'PARAM1_NUMBER';
-    } else if (commandLine.match(/^\d+\s->/)) {
-        command = 'INIT';
-    } else if (commandLine.match(/^[a-z]+\s->/)){
-        command = 'FORWARD';
-    } else {
-            command = commandLine.match(/[A-Z]+/)![0];
+const COMMAND_REGEX = /[A-Z]+/g;
+const ARGUMENT_REGEX = /[a-z0-9]+/g;
+const wires = new Map();
 
-    }
+export function day7(input: string, resultWire: string) {
 
-    //console.log(commandLine)
-    /*console.log(commandLine.match(/\w+$/))
-    console.log(commandLine.match(/\w+$/)[0])*/
-    const target = commandLine.match(/\w+$/)![0];
-    /*if (target === 't') {
-        console.log(commandLine)
+
+    /*for (const line of input.split('\n')) {
+
+        const parsedOperation = operationParser(line);
+        wires.set(parsedOperation.target, {command: parsedOperation.command, args: parsedOperation.args});
     }*/
-    let firstParam;
-    let secondParam;
-    let source;
-    let step;
 
-    switch (command) {
 
-        case 'AND':
-            firstParam = commandLine.match(/^\w+/)![0];
-            secondParam = commandLine.match(/\w+(?=\s->)/)![0];
-            if (values.has(firstParam) && values.has(secondParam)) {
-                return {param1: firstParam, param2: secondParam, command: command, target: target};
-            }
-            break;
-
-        case 'OR':
-            firstParam = commandLine.match(/^\w+/)![0];
-            secondParam = commandLine.match(/\w+(?=\s->)/)![0];
-            if (values.has(firstParam) && values.has(secondParam)) {
-                return {param1: firstParam, param2: secondParam, command: command, target: target};
-            }
-            break;
-
-        case 'NOT':
-            source = commandLine.match(/\w+(?=\s->)/)![0];
-            if (values.has(source)) {
-                return {param1: source, command: command, target: target};
-            }
-            break;
-
-        case 'LSHIFT':
-            source = commandLine.match(/^\w+/)![0];
-            step = commandLine.match(/(?<=SHIFT\s)\w/)![0];
-            if (values.has(source)) {
-                return {param1: source, param2: step, command: command, target: target};
-            }
-            break;
-
-        case 'RSHIFT':
-            source = commandLine.match(/^\w+/)![0];
-            step = commandLine.match(/(?<=SHIFT\s)\w/)![0];
-            if (values.has(source)) {
-                return {param1: source, param2: step, command: command, target: target};
-            }
-            break;
-
-        case 'FORWARD':
-            source = commandLine.match(/^\w+/)![0];
-            if (values.has(source)) {
-                return {param1: source, command: command, target: target};
-            }
-            break;
-
-        case 'PARAM1_NUMBER':
-            firstParam = '1';
-            secondParam = commandLine.match(/\w+(?=\s->)/)![0];
-            if (values.has(secondParam)) {
-                return {param1: firstParam, param2: secondParam, command: command, target: target};
-            }
-            break;
-
-        case 'INIT':
-            const initValue = +commandLine.match(/^\d+/)![0];
-
-            return {param1: initValue.toString(), command: command, target: target};
-
-        default:
-            console.log('default ág')
-    }
+    initWires(input);
+    calculateValue(resultWire);
+    const part1Result = wires.get(resultWire);
+    initWires(input);
+    wires.set('b', part1Result);
+    calculateValue(resultWire);
+    const part2Result = wires.get(resultWire);
+    //console.log(wires)
+    return {part1: part1Result, part2: part2Result};
 }
 
-function commandProcessor(command: Command, values: Map<string, number>) {
-    if (command.command === undefined) {
-        console.log('nincs')
-    }
 
-    switch (command.command) {
-        case 'AND':
-            values.set(command.target, values.get(command.param1)! & values.get(<string>command.param2)!)
-            break;
-
-        case 'OR':
-            values.set(command.target, values.get(command.param1)! | values.get(<string>command.param2)!)
-            break;
-
-        case 'NOT':
-            if (command.param2 === undefined) {
-                //console.log('para2')
-            }
-            //console.log(command.target + ', ' + command.param1 + ', ' + command.param2)
-            values.set(command.target, new Uint16Array([~values.get(command.param1)!])[0]);
-            break;
-
-        case 'LSHIFT':
-            values.set(command.target, values.get(command.param1)! << +command.param2!);
-            break;
-
-        case 'RSHIFT':
-            values.set(command.target, values.get(command.param1)! >> +command.param2!);
-            break;
-
-        case 'FORWARD':
-            values.set(command.target, values.get(command.param1)!);
-            break;
-
-        case 'PARAM1_NUMBER':
-            values.set(command.target, +command.param1 & values.get(<string>command.param2)!);
-            break;
-
-        case 'INIT':
-            values.set(command.target, +command.param1);
-            break;
-    }
+function initWires(input: string) {
+    input.split('\n').forEach(instruction => {
+        const parsedInstruction = operationParser(instruction);
+        wires.set(parsedInstruction.target, {command: parsedInstruction.command, args: parsedInstruction.args});
+    });
 }
+
+
+function calculateValue(wireName: any) {
+
+    const wire = wires.get(wireName);
+
+    if (typeof wireName === 'number') return wireName;
+    if (typeof wire === 'number') return wire;
+    if (typeof wire === 'undefined') return undefined;
+    //console.log(wire)
+    if (!wire.command) {
+        wires.set(wireName, calculateValue(wire.args[0]));
+    } else {
+        //console.log('command: ' + wire.command)
+        wires.set(wireName, METHODS[wire.command](calculateValue(wire.args[0]), calculateValue(wire.args[1])));
+    }
+
+    return wires.get(wireName);
+}
+
+
+function operationParser(line: any): Operation {
+
+    const args = line.match(ARGUMENT_REGEX)!;
+    const command = line.match(COMMAND_REGEX)!;
+    const target = args.pop()!;
+
+    return {target: target, args: args.map(arg => isNaN(Number(arg)) ? arg : Number(arg)), command: command && command[0]};
+}
+
+
