91789f308f92bd941199a900387a6ae79037ef6b
Dudik Pál <dudikpal@gmail.com>
2022-01-29 23:07:44 +0100

    #3: 2015 Day7 refactoring: - remove red underline because type is missing

--- 2015/Day7/day7.ts
+++ 2015/Day7/day7.ts
@@ -5,12 +5,12 @@ type Operation = {
 }
 
 
-const METHODS = {
-    AND: (a, b) => (a & b),
-    OR: (a, b) => (a | b),
-    NOT: a => new Uint16Array([~a])[0],
-    LSHIFT: (a, b) => (a << b),
-    RSHIFT: (a, b) => (a >> b)
+const METHODS: any = {
+    AND: (a: any, b: any) => (a & b),
+    OR: (a: any, b: any) => (a | b),
+    NOT: (a: any) => new Uint16Array([~a])[0],
+    LSHIFT: (a: any, b: any) => (a << b),
+    RSHIFT: (a: any, b: any) => (a >> b)
 };
 
 const COMMAND_REGEX = /[A-Z]+/g;
@@ -19,22 +19,15 @@ const wires = new Map();
 
 export function day7(input: string, resultWire: string) {
 
-
-    /*for (const line of input.split('\n')) {
-
-        const parsedOperation = operationParser(line);
-        wires.set(parsedOperation.target, {command: parsedOperation.command, args: parsedOperation.args});
-    }*/
-
-
     initWires(input);
     calculateValue(resultWire);
     const part1Result = wires.get(resultWire);
+
     initWires(input);
     wires.set('b', part1Result);
     calculateValue(resultWire);
     const part2Result = wires.get(resultWire);
-    //console.log(wires)
+
     return {part1: part1Result, part2: part2Result};
 }
 
@@ -54,11 +47,10 @@ function calculateValue(wireName: any) {
     if (typeof wireName === 'number') return wireName;
     if (typeof wire === 'number') return wire;
     if (typeof wire === 'undefined') return undefined;
-    //console.log(wire)
+
     if (!wire.command) {
         wires.set(wireName, calculateValue(wire.args[0]));
     } else {
-        //console.log('command: ' + wire.command)
         wires.set(wireName, METHODS[wire.command](calculateValue(wire.args[0]), calculateValue(wire.args[1])));
     }
 
@@ -72,7 +64,7 @@ function operationParser(line: any): Operation {
     const command = line.match(COMMAND_REGEX)!;
     const target = args.pop()!;
 
-    return {target: target, args: args.map(arg => isNaN(Number(arg)) ? arg : Number(arg)), command: command && command[0]};
+    return {target: target, args: args.map((arg: string | number) => isNaN(Number(arg)) ? arg : Number(arg)), command: command && command[0]};
 }
 
 
